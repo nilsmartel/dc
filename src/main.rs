fc9bb123188c24efba5c728992c497c977761e5b
Nils Martel <nils.martel@concepts-system.de>
2019-02-25 18:06:10 +0100

    Binch of useful stuff added

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 mod io;
+mod state_machine;
 
 fn main() {
     let code = if let Some(filename) = std::env::args().nth(1) {
-        io::read_file(&filename).expect("Failed to read file");
+        io::read_file(&filename).expect("Failed to read file")
     } else {
-        io::read_stdin();
+        io::read_stdin()
     };
+
+    let mut state = state_machine::State::from_sequence(code.lines());
 }
--- src/state_machine.rs
+++ src/state_machine.rs
@@ -1,6 +1,9 @@
-pub struct State {
+pub struct State<'a, T>
+where
+    T: Iterator<Item = &'a str>,
+{
     stack: Vec<i32>,
-    instruction_sequence: Iterator<Item = &str>,
+    instruction_sequence: T,
 }
 
 enum Instruction {
@@ -8,6 +11,7 @@ enum Instruction {
     Operation(Operator),
 }
 
+#[derive(Copy, Clone)]
 enum Operator {
     Add,
     Sub,
@@ -16,25 +20,70 @@ enum Operator {
     Mod,
 }
 
-impl Stack {
-    pub fn from_sequence(instruction_sequence: Iterator<Item = &str) -> State {
-        State { stack: Vec::new(), instruction_sequence }
+impl Operator {
+    fn operate<'a, T>(self, state: &mut State<'a, T>) -> i32
+    where
+        T: Iterator<Item = &'a str>,
+    {
+        let input: Vec<i32> = (0..self.required_input_count())
+            .map(|_| state.next().expect("Unexpected empty Stack"))
+            .collect();
+
+        match self {
+            Operator::Add => input.iter().sum(),
+            Operator::Sub => input[1] - input[0],
+            Operator::Mul => input[1] * input[0],
+            Operator::Div => input[1] / input[0],
+            Operator::Mod => input[1] % input[0],
+        }
     }
 
-    fn next_instruction(&mut self) -> Option<Instruction> {
-        if let Some(s) = (self.instruction_sequence.next() {
-            let res = match s.trim_whitespace() {
+    fn required_input_count(self) -> usize {
+        2
+    }
+}
+
+impl<'a, T> State<'a, T>
+where
+    T: Iterator<Item = &'a str>,
+{
+    pub fn from_sequence(instruction_sequence: T) -> State<'a, T> {
+        State {
+            stack: Vec::new(),
+            instruction_sequence,
+        }
+    }
+
+    fn parse_instruction(&mut self) -> Option<Instruction> {
+        if let Some(s) = self.instruction_sequence.next() {
+            let res = match s.trim() {
                 "+" => Instruction::Operation(Operator::Add),
                 "-" => Instruction::Operation(Operator::Sub),
                 "*" => Instruction::Operation(Operator::Mul),
-                "(" => Instruction::Operation(Operator::Div),
-                "%" => Instruction::Operation(Operator::Mod),
-                n => n.parse::<i32>()
+                "/" => Instruction::Operation(Operator::Div),
+                "%" => Instruction::Operation(Operator::Mul),
+                n => Instruction::Value(n.parse::<i32>().expect("Failed to parse number")),
             };
 
             return Some(res);
         }
 
         None
+    }
+
+    pub fn next(&mut self) -> Option<i32> {
+        loop {
+            match self.parse_instruction() {
+                None => return None,
+                Some(instruction) => match instruction {
+                    Instruction::Value(x) => self.stack.push(x),
+                    Instruction::Operation(op) => {
+                        let result = op.operate(&mut self);
+                        self.stack.push(result);
+                        return Some(result);
+                    }
+                },
+            };
+        }
     }
 }
